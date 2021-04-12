cb506c416cbf600f5ec34842822f339d65c415d0
EaswarUS <80040514+EaswarUS@users.noreply.github.com>
2021-04-12 14:54:02 +0530

    Update RemoveDuplicates.java

--- week1/day2/assignments/RemoveDuplicates.java
+++ week1/day2/assignments/RemoveDuplicates.java
@@ -5,6 +5,21 @@ public class RemoveDuplicates {
 	public static void main(String[] args) {
      
 		String text = "We learn java basics as part of java sessions in java week1";
+		String[] split = text.split(" ");
+		int length = split.length;
+		for (int i = 0; i < length; i++) {
+			for (int j = i+1; j < length; j++) {
+				if (split[i].equals(split[j])) {
+					
+					split[j]="";	
+				}
+				
+			}
+			System.out.print(split[i]+ " ");
+		}
+		
+		
+		
 	}
 
 }
